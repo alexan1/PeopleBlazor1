e7f7a3105624ea1b6241957f548e28749829dd82
Alexandre Konioukhov <alexan1@gmail.com>
2020-01-12 19:13:52 -0500

    GetWikiPerson

--- WikiLib/Wiki.cs
+++ WikiLib/Wiki.cs
@@ -28,20 +28,16 @@ namespace WikiLib
             var root = doc.RootElement;
             var entities = root.GetProperty("results").GetProperty("bindings");
 
-            var FoundPersons = new Collection<WikiPerson>();
-                      
+            var FoundPersons = new Collection<WikiPerson>();                      
             //var count = entities.Count();
-
            
             foreach (var item in entities.EnumerateArray())
             {
-
                 int id = 0;
                 if (item.TryGetProperty("item", out JsonElement itemId))
                 {
                     int.TryParse(itemId.GetProperty("value").ToString().Substring(32), out id);
-                }
-                                
+                }                                
 
                 var name = String.Empty;
                 if (item.TryGetProperty("itemLabel", out JsonElement itemLabel))
@@ -52,7 +48,7 @@ namespace WikiLib
                 var description = String.Empty;
                 if (item.TryGetProperty("itemDescription", out JsonElement itemDescription))
                 {
-                    name = itemDescription.GetProperty("value").ToString();
+                    description = itemDescription.GetProperty("value").ToString();
                 }
 
                 DateTime birthday = DateTime.MinValue;
@@ -60,7 +56,6 @@ namespace WikiLib
                 {
                     DateTime.TryParse(DR.GetProperty("value").ToString(), out birthday);                    
                 }
-
 
                 DateTime death = DateTime.MinValue;
                 if (item.TryGetProperty("RIP", out JsonElement RIP))
@@ -78,8 +73,7 @@ namespace WikiLib
                 if (item.TryGetProperty("article", out JsonElement article))
                 {
                     link = article.GetProperty("value").ToString();
-                }
-                                
+                }                                
 
                 var person = new WikiPerson
                 {
@@ -92,12 +86,79 @@ namespace WikiLib
                     Link = link,
                     //Rating = rating
                 };
-                FoundPersons.Add(person);
-                
+                FoundPersons.Add(person);                
+            }
+            return FoundPersons;
+        }
+
+        public async static Task<WikiPerson> GetWikiPerson(int id)
+        {
+            var urlBase = "https://query.wikidata.org/sparql";
+            var query = "SELECT DISTINCT  ?item ?itemLabel ?itemDescription (SAMPLE(?DR) AS ?DRSample) (SAMPLE(?article) AS ?articleSample)" +
+            "WHERE{ ?article  schema:about ?item ; schema:inLanguage  'en' ; schema:isPartOf    <https://en.wikipedia.org/>" +
+            "FILTER ( ?item = <http://www.wikidata.org/entity/Q937> )" +
+            "OPTIONAL { ?item  wdt:P569  ?DR }" +
+            "OPTIONAL { ?item  wdt:P570  ?RIP }" +
+            "OPTIONAL { ?item  wdt:P18  ?image }" +
+            "SERVICE wikibase:label { bd:serviceParam wikibase:language  'en'}}" +
+            "GROUP BY ?item ?itemLabel ?itemDescription";
+            var url = urlBase + "?query=" + query + "&format=json";
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36");
+            var json = await client.GetStringAsync(url);
+            var doc = JsonDocument.Parse(json);
+            var root = doc.RootElement;
+            var item = root.GetProperty("results").GetProperty("bindings");
+
+            var name = String.Empty;
+            if (item.TryGetProperty("itemLabel", out JsonElement itemLabel))
+            {
+                name = itemLabel.GetProperty("value").ToString();
             }
 
+            var description = String.Empty;
+            if (item.TryGetProperty("itemDescription", out JsonElement itemDescription))
+            {
+                description = itemDescription.GetProperty("value").ToString();
+            }
 
-            return FoundPersons;
+            DateTime birthday = DateTime.MinValue;
+            if (item.TryGetProperty("DR", out JsonElement DR))
+            {
+                DateTime.TryParse(DR.GetProperty("value").ToString(), out birthday);
+            }
+
+            DateTime death = DateTime.MinValue;
+            if (item.TryGetProperty("RIP", out JsonElement RIP))
+            {
+                DateTime.TryParse(RIP.GetProperty("value").ToString(), out death);
+            }
+
+            var image = string.Empty;
+            if (item.TryGetProperty("image", out JsonElement imageE))
+            {
+                image = imageE.GetProperty("value").ToString();
+            }
+
+            var link = String.Empty;
+            if (item.TryGetProperty("article", out JsonElement article))
+            {
+                link = article.GetProperty("value").ToString();
+            }
+
+            var person = new WikiPerson
+            {
+                Id = id,
+                Name = name,
+                Description = description,
+                Birthday = birthday,
+                Death = death,
+                Image = image,
+                Link = link,
+                //Rating = rating
+            };
+
+            return person;
         }
     }
 }
--- WikiTest/WikiTests.cs
+++ WikiTest/WikiTests.cs
@@ -10,9 +10,15 @@ namespace WikiTest
         public void WikiSearch1()
         {
             var people = Wiki.WikiSearch("Pope");
-
             Assert.AreNotEqual(0, people.Result.Count);
+        }
 
+        [TestMethod]
+        public void GetPerson1()
+        {
+            var person = Wiki.GetWikiPerson(303).Result;
+
+            Assert.IsNotNull(person);
         }
     }
 }
