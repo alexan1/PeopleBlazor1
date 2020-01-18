d4c4d6e1da0132d2fed6db32b4bf92672ef4a765
Alexandre Konioukhov <alexan1@gmail.com>
2020-01-18 17:01:09 -0500

    balzor pages

---
+++ PeopleBlazor1/Pages/Index.razor.cs
@@ -0,0 +1,28 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+using System.Collections.ObjectModel;
+
+namespace PeopleBlazor1.Pages
+{
+    public partial class Index
+    {
+        [Inject]
+        private HttpClient Http { get; set; }
+        protected Collection<Person> People { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            People = await Http.GetJsonAsync<Collection<Person>>("https://people3api.azurewebsites.net/api/People");
+        }
+
+        protected class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            //public int Rate { get; set; }
+        }
+    }
+}
---
+++ PeopleBlazor1/Pages/Search.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeopleBlazor1.Pages
+{
+    public partial class Search
+    {
+    }
+}
